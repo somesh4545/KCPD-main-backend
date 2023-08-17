7a3e34c1ddfde9bd027ef30f7151e54d43359114
Somesh <someshsomani457@gmail.com>
2023-08-17 10:03:07 +0530

    Add get_all_games route.

--- routes/general.py
+++ routes/general.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, HTTPException, Response
-from models.index import PLAYERS, DOCUMENTS, TEAMS, TEAM_PLAYERS, USERS
-from schemas.index import Player, Document, Login, Teams, TeamPlayers,GenericResponseModel
+from models.index import GAMES
+from schemas.index import GenericResponseModel
 from sqlalchemy.orm import Session, joinedload, load_only
 from fastapi import Depends
 from config.db import get_db
@@ -18,13 +18,11 @@ generalRouter = APIRouter()
 async def get_fixture_by_id(fixture_id:int,user_id: str=Depends(get_current_user), db: Session = Depends(get_db)):
     return Tournament_Game_Service(db).get_fixture_by_id(fixture_id, user_id)
 
-# @generalRouter.get('/team/{team_id}')
-# async def get_team_by_id(team_id: str, user_id: str=Depends(get_current_user), db: Session = Depends(get_db)):
-#     team = db.query(TEAMS).options(
-#         joinedload(TEAMS.admin).load_only(USERS.first_name, USERS.email_id),
-#         joinedload(TEAMS.team_players).load_only(TEAM_PLAYERS.id).joinedload(TEAM_PLAYERS.player).load_only(USERS.first_name, USERS.email_id)
-#     ).filter(TEAMS.id == team_id).first()
-#     if team is None:
-#         return GenericResponseModel(status='error', message='Invalid team id passed', status_code=http.HTTPStatus.BAD_REQUEST)
+@generalRouter.get('/get_all_games')
+async def get_all_games(db: Session = Depends(get_db)):
+    pass
+    games = db.query(GAMES).all()
 
-#     return {'status': 'success', 'message': 'Team details', 'status_code': http.HTTPStatus.OK, 'data': team}
+    result = [{'id': g.id, 'text': g.name, 'value': g.name} for g in games]
+
+    return result
